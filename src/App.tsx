dbf3b15958724d46b835cd28216b0f68b2d1b974
ekater1na <ekaterina_l@tut.by>
2022-09-28 19:16:52 +0300

    feat: update tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-
+import React, { createElement as e, useState } from "react";
 
 function App() {
-  return (
-    <h1 className="container">Hi React</h1>  
-  );
+  const [count, setCount] = useState(0);
+
+  return e("div", { className: "container" }, [
+    e("h1", { className: "text-3xl font-bold underline", key: 0 }, `Test ${count}`),
+    e(
+      "button",
+      {
+        className: "py-2 px-4 border",
+        key: 1,
+        onClick: () => setCount(count+1),
+      },
+      "Hi React"
+    ),
+  ]);
 }
 
 export default App;
