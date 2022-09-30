c8eeb92fe551dcccf0d516fb884037919cfe6cd5
ekater1na <ekaterina_l@tut.by>
2022-09-30 21:55:44 +0300

    feat: toggle button

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import { IProduct } from '../models';
+import React, { useState } from "react";
+import { IProduct } from "../models";
 
 interface ProductProps {
-    product: IProduct
+  product: IProduct;
 }
 
-export function Product(props: ProductProps) {
-    console.log(props.product.category);
-  return (    
-    <div className="container mx-auto">
-        <span className="text-gray-500 sm:text-sm">`Product ${props.product.title}`</span>      
+export function Product({ product }: ProductProps) {
+  const [details, setDetails] = useState(false);
+
+  const btnBgClassName = details ? "bg-yellow-400" : "bg-blue-400";
+  const btnClasses = ["py-2 px-4 border", btnBgClassName];
+
+  return (
+    <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+      <img src={product.image} alt={product.title} className="w-1/3" />
+      <p>{product.title}</p>
+      <p className="font-bold">{product.price}</p>
+
+      <button
+        className={btnClasses.join(" ")}
+        onClick={() => setDetails((prev) => !prev)}
+      >
+        {details ? "Hide details" : "Show details"}
+      </button>
+
+      {details && (
+        <div>
+          <p>{product.description}</p>
+          <p>Rate: <span style={ {fontWeight: 'bold'} } >{product.rating.rate}</span></p>
+        </div>
+      )}
     </div>
   );
 }
