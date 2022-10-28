1c813200e8eb0e3af4e8ff98c09582f7745c8332
ekater1na <ekaterina_l@tut.by>
2022-10-28 18:49:05 +0300

    feat: add basic routing

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function AboutPage(){
+    return(
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste esse ab praesentium repellat neque ipsum nam deserunt quam magni enim ad qui, ducimus, beatae velit at. Est, tempore quo. In.</div>
+    )
+}
